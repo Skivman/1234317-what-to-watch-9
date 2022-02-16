984f7f29211d1e15933af317c6b1a511fe0c8298
Алексей Ситкевич <al.sitkevich@gmail.com>
2022-02-16 12:43:39 +0300

    2.11. Начинаем программировать _4

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  FILM_TITLE: 'The Grand Budapest Hotel',
+  FILM_GENRE: 'Drama',
+  FILM_YEAR: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmTitle = {Setting.FILM_TITLE} filmGenre = {Setting.FILM_GENRE} filmYear = {Setting.FILM_YEAR}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
